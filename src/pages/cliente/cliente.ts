5bc511b9adde0339d0ec6318d9f4555d8745a539
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2018-08-29 16:26:08 -0500

    CocinaPage implementation

--- src/pages/cliente/cliente.ts
+++ src/pages/cliente/cliente.ts
@@ -6,6 +6,7 @@ import {
 } from "/Users/mac/Proyectos/restaurante-sockets/src/pages/providers/order/order";
 import { Observable } from "/Users/mac/Proyectos/restaurante-sockets/node_modules/rxjs/Rx";
 import { PedidoPage } from "../pedido/pedido";
+import { CocinaPage } from "../cocina/cocina";
 //import { FormBuilder,FormGroup, Validators } from "@angular/forms";
 
 @IonicPage()
